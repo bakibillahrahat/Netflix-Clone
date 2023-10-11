b3fc167cfbde0aac7c75a2d29f8c03df6fb8b828
bakibillahrahat <bakibillahrahat@protonmail.com>
2023-10-12 02:57:36 +0600

    Add nav button

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -12,7 +12,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
     }
     
 }
--- Netflix Clone/Controllers/UpComingViewController.swift
+++ Netflix Clone/Controllers/UpComingViewController.swift
@@ -12,7 +12,7 @@ class UpComingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .green
+        view.backgroundColor = .systemBackground
     }
 
 }
