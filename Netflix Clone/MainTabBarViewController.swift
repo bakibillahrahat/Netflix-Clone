db7aa152347434e4cda967f68d293ef8ac281e66
MD. Bakibillah Rahat <mdbakibillahrahat@proton.me>
2023-10-11 22:20:24 +0600

    add background color

--- Netflix Clone/MainTabBarViewController.swift
+++ Netflix Clone/MainTabBarViewController.swift
@@ -12,6 +12,7 @@ class MainTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemYellow
     }
 
 
